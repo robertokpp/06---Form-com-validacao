95a97462606bc61ef58380cd47226a7afdfd2ac1
Roberto Kennedy <roberto.kpp@gmail.com>
2026-01-01 15:15:33 -0500

    Ajusta validação de campos no formulário e corrige largura do formulário

--- script.js
+++ script.js
@@ -1,22 +1,66 @@
-// const name = document.getElementById("name")
-// const email = document.getElementById("email")
-// const subject = document.getElementById("subject")
-// const menssage = document.getElementById("menssage")
-// const btnSubmit = document.getElementById("btn-submit")
+const form = document.querySelector("form");
+const formName = form.querySelector("#name");
+const email = form.querySelector("#email");
+const subject = form.querySelector("#subject");
+const message = form.querySelector("#message");
 
-const form = document.querySelector("form")
-
-function validation(element) {
-  
+function validacion(params, params2) {
+  const element = params.querySelector("span");
+  if (element) {
+    return;
+  } else {
+    const span = document.createElement("span");
+    const label = params.querySelector("label");
+    span.classList = "alert";
+    if (params2 === undefined) {
+      span.textContent = `Ops! O ${label.textContent} ficou em branco.`;
+    } else {
+      span.textContent = `Ops! O E-mail é invalido `;
+    }
+    params.append(span);
+  }
 }
 
-form.addEventListener("submit", (e)=>{
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const name = form.querySelector("#name")
-
-  console.log(name.value)
-  if(name.value.trim() === ""){
-    
+  let i = 0;
+  if (formName.value.trim() === "") {
+    const div = formName.parentElement;
+    validacion(div);
+  } else {
+    i++;
   }
-})
+  if (email.value.trim() === "") {
+    const div = email.parentElement;
+    validacion(div);
+  } else if (!email.value.includes(".com")) {
+    const div = email.parentElement;
+    validacion(div, "invalido");
+  } else {
+    i++;
+  }
+  if (subject.value.trim() === "") {
+    const div = subject.parentElement;
+    validacion(div);
+  } else {
+    i++;
+  }
+  if (message.value.trim() === "") {
+    const div = message.parentElement;
+    validacion(div);
+  } else {
+    i++;
+  }
 
+  
+  if (i === 4) {
+    formName.value = "";
+    email.value = "";
+    subject.value = "";
+    message.value = "";
+    const clearAlert = form.querySelectorAll(".alert");
+    clearAlert.forEach(element => {
+      element.remove()
+    }); 
+  }
+});
